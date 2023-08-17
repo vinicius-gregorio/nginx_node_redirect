ce3786c7a35eb0b6800f774ab38e465d218ddfaa
Vinicius Gregorio <vincamgreg@hotmail.com>
2023-08-17 15:03:53 -0300

    feat: add node module

--- node/index.js
+++ node/index.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
 const port = 3000;
 
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const conn = mysql.createConnection(config);
+
+const sqlInsert = `INSERT INTO person(name) values('Jonathan Joestar')`;
+const showQuery = `SELECT * FROM person`;
+
+conn.query(sqlInsert);
+
+const people = conn.query(showQuery);
+console.log(people);
+conn.end();
 app.get("/", (req, res) => {
   res.send("<h1>Docker Container</h1>");
 });
